e98bd44d5a4f2ffbb48c840e49444d10d413c7a7
Justin00707 <Justinsauv@gmail.com>
2024-01-29 20:02:32 -0500

    Your commit message

---
+++ Downloads/FitnessHeat_API-main/Models/Souscription.js
@@ -0,0 +1,43 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../config/database');
+
+sequelize.sync();
+
+const Souscription = sequelize.define(
+  'souscription',
+  {
+    ID_Souscription: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    DateSouscription: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    Actif: {
+      type: Sequelize.BOOLEAN, // Assuming Actif is a boolean field
+      allowNull: false,
+    },
+    ID_Utilisateur: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Utilisateur',
+        key: 'ID_Utilisateur',
+      },
+    },
+    ID_Programme: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Programme',
+        key: 'ID_Programme',
+      },
+    },
+  },
+  {
+    timestamps: false,
+    tableName: 'souscriptions',
+  }
+);
+
+module.exports = Souscription;
